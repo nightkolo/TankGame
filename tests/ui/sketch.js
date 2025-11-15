d571824e1b45000b9fb66741c0b0202752826a44
Kolo <thenitezune104@gmail.com>
2025-11-15 19:05:15 +0400

    Sad commit
    Early p5.touchgui implemntation

---
+++ tests/ui/sketch.js
@@ -0,0 +1,48 @@
+// import { Font } from "../../../../../../.vscode/extensions/samplavigne.p5-vscode-1.2.16/p5types";
+
+// It kinda sucks when you don't have that creative energy :/
+
+let gui;
+let f;
+
+function preload(){
+  f = loadFont("font/Nunito-Bold.ttf");
+}
+
+function setup() {
+
+
+  createCanvas(400, 400);
+
+  let w = 100;
+  let h = 32;
+  gui = createGui();
+  b = createButton("Start", (width/2) - w/2, 50, w, h);
+  b2 = createButton("Start", (width/2) - w/2, 100, w, h);
+
+  b.labelOn = "Bad idea.";
+  b.setStyle({
+      fillBg: color("#fbbfbfff"),
+      font: f,
+      rounding: 5,
+      textSize: 20
+  });
+
+}
+
+
+
+function draw() {
+  background(220);
+  textFont("Nunito");
+  textStyle(BOLD);
+
+  textSize(25);
+  text("Hello there. y", width/2, height/2);
+  
+  if(b.isPressed) {
+    print(b.label + " is pressed.");
+  }
+
+  drawGui();
+}
